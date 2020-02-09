f27abf15a6a66b99fad4f935652356c12eef4aef
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2020-02-08 22:23:17 -0600

    Metodo corte, falta implementar ssd y alinear

--- test.py
+++ test.py
@@ -46,17 +46,68 @@ def select():
             cv2.destroyAllWindows()
     generalFormula()
     converVideo()
-    cv2.imshow('Frame Final',img_array[-1])
-    cv2.waitKey()
+    imgAdd()
+
+def imgAdd():
+    example = "resultado"+str(len(img_array)-1)+".jpg"
+    print(f'VALOR EXAMPLE {example}')
+    # Carga de imagen y agregación de evento al mouse
+    image = cv2.imread(example)
+    clone = image.copy()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", shape_selection)
+
+    while True:
+        # display the image and wait for a keypress
+        cv2.imshow("image", image)
+        key = cv2.waitKey(1) & 0xFF
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("s"):
+            image = clone.copy()
+            break
+    # Se muestra la imagen recortada
+    if len(ref_point) == 2:
+        crop_img = clone[ref_point[0][1]:ref_point[1]
+                        [1], ref_point[0][0]:ref_point[1][0]]
+        cv2.imwrite("corte.jpg", crop_img)
+        cv2.imshow("crop_img", crop_img)
+        cv2.waitKey(0)
+        print(f'CROP IMAGE {crop_img}')
+
+def shape_selection(event, x, y, flags, param):
+    example = "resultado"+str(len(img_array)-1)+".jpg"
+    image = cv2.imread(example)
+    # grab references to the global variables
+    global ref_point, cropping
+
+    # Seleccion de coordenadas ok
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ref_point = [(x, y)]
+        cropping = True
+        
+    # Seleccion de coordenadas No ok
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Seleccion x,y
+        # Termina corte
+        ref_point.append((x, y))
+        cropping = False
+
+        # Rectangulo
+        cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2)
+        cv2.imshow("image", image)
 
 if __name__ == '__main__':
 
     arrayImg = []
     img_array = []
     alpha=0.97
+    ref_point = []
+    cropping = False
+    example=0
+    img=0
     
     path = Tk()
-    path.geometry('200x200')
+    path.geometry('100x100')
     path.configure(bg = 'azure')
     path.title('Selección de video')
     ttk.Button(path, text='Seleccionar video', command=select ).pack(side=BOTTOM)
